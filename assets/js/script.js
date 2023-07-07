1825189cb4774c5c8134c2ab3298f7ac45e1ad63
RyanTheScholar <Ryan.business.bowen@gmail.com>
2023-07-07 14:51:55 -0500

    added search bar and header to the .html

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// esrb rating is a query parameter
+// no games above mature
+var queryURL = 'https://api.rawg.io/api/games/' + inputval + '?key='+ rawgKey;
+var rawgKey ='9291f496b0954cfd85fdd080b9cd538f';
+
+
